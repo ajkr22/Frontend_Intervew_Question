163101aa915c4f068d0b5dfc4a42f86889cd7be8
Ajay Kumar <48029391+ajkr22@users.noreply.github.com>
2022-03-03 18:14:19 +0530

    Create QuestionBank2.js

---
+++ QuestionBank2.js
@@ -0,0 +1,3 @@
+Clone with filter functionality?
+Website: https://wforwomanonline.com/category/top-wear-kurtas.html
+Json data: https://pim.wforwoman.com/pim/pimresponse.php/?service=category&store=1&url_key=top-wear-kurtas&page=1&count=20&so
