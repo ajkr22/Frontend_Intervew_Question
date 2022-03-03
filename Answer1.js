f249eb3d942b744b36637ecf212b7c09e4058dd5
Ajay Kumar <48029391+ajkr22@users.noreply.github.com>
2022-03-03 17:43:55 +0530

    Create Answer1.js

---
+++ Answer1.js
@@ -0,0 +1,26 @@
+["",null,1,0,false,true,"true"].filter((item)=>{ return item })
+
+=>1,true,'true'
+
+Q.Write a program to remove duplicate?
+const color = ["red","green","orange","white","black","orange","cyan"]
+
+
+Q.Print the output?
+(function() {
+    console.log(1); 
+    setTimeout(function(){console.log(2)}, 1000); 
+    setTimeout(function(){console.log(3)}, 0); 
+    console.log(4);
+})();
+
+Q.Write a program to compare ?
+{name : 'gaurav',age:22} === {name: 'gaurav',age:22}
+
+function shallowEqual(object1, object2) {
+  
+}
+
+What is the output?
+10 + "1" - 1
+=>100
