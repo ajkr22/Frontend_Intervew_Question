ed442ae736abdbc2e89fb0b6e9d53b6f1d0b654f
Ajay Kumar <48029391+ajkr22@users.noreply.github.com>
2022-03-03 17:41:24 +0530

    Create QuestionBank1.js

---
+++ QuestionBank1.js
@@ -0,0 +1,20 @@
+["",null,1,0,false,true,"true"].filter((item)=>{ return item })
+
+const color = ["red","green","orange","white","black","orange","cyan"]
+
+
+(function() {
+    console.log(1); 
+    setTimeout(function(){console.log(2)}, 1000); 
+    setTimeout(function(){console.log(3)}, 0); 
+    console.log(4);
+})();
+
+{name : 'gaurav',age:22} === {name: 'gaurav',age:22}
+
+Write a program to compare the object ?
+function shallowEqual(object1, object2) {
+  
+}
+
+10 + "1" - 1
